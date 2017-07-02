2a25462bcc25be3fc8790bbde6587972f0c339eb
Ihor Sokoliuk <yaaappee@gmail.com>
2017-07-02 22:50:13 +0300

    Add User controller

---
+++ EverMoney/Server.WebAPI/Controller/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Server.DataAccess;
+using Server.DataAccess.Context;
+using Server.DataAccess.Model;
+
+namespace Server.WebAPI.Controller
+{
+    [Route("api/[controller]")]
+    public class UserController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        private readonly SecurityContext _securityContext;
+
+        public UserController()
+        {
+            _securityContext = ContextFactory.SecurityContext;
+        }
+
+        // GET: /api/user?login=...&password=...  
+        [HttpGet]
+        public IEnumerable<User> Get(string login, string password)
+        {
+            return _securityContext.Users.Where(a => a.Account.Login == login && a.Account.Password == password);
+        }
+    }
+}
