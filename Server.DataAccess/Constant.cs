aa4c68568fdce7309d5c5c2d874992f5aa70dfe7
DESKTOP-TS088FI\Antony <semanton7@gmail.com>
2017-09-07 12:53:00 +0300

    Init project 2

--- Server.DataAccess/Constant.cs
+++
@@ -1,9 +0,0 @@
-namespace Server.DataAccess
-{
-    public static class Constant
-    {
-        // TODO for Antony: Add security here
-        public static string ConnectionString =>
-            "Data Source=localhost;Initial Catalog=EverMoney;Integrated Security=true;";
-    }
-}
